6db96a8d1bc1375794d48d359b62080391c0e771
Alvina Putri <nivni91@gmail.com>
2015-09-03 22:47:51 +0700

    fix tab vertical js

--- src/js/tab.js
+++ src/js/tab.js
@@ -1,11 +1,15 @@
-$(".tab-container .tab-content").hide();
-$(".tab-container .tab-content").first().show();
+$(".tab-container .tab-contents .tab-content").hide();
+$(".tab-container.tab-vertical .tab-contents .tab-content").hide();
+$(".tab-container .tab-contents .tab-content").first().show();
+$(".tab-container.tab-vertical .tab-contents .tab-content").first().show();
 $(".tab-container .nav-tabs li").first().addClass('active');
+$(".tab-container.tab-vertical .nav-tabs li").first().addClass('active');
 $(".tab-container .nav-tabs li a").click(function(e){
     e.preventDefault();
     var tabTarget = $(this).parent().data('tab');
-    $(this).parent().parent().siblings(".tab-content").hide();
-    $(this).parent().parent().siblings(".tab-content#tab-content-"+tabTarget).show();
+    // Normal tabs
+    $(this).parent().parent().siblings('.tab-contents').children(".tab-content").hide();
+    $(this).parent().parent().siblings('.tab-contents').children(".tab-content#tab-content-"+tabTarget).show();
     $(this).parent().siblings('li').removeClass('active');
     $(this).parent().addClass('active');
 });
