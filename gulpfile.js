f11aebc90792d48930eb820ff09bbed987846214
Alvina Putri <nivni91@gmail.com>
2015-10-09 00:14:42 +0700

    gulp build system

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var renames = require('gulp-rename');
+var concats = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+
+
+gulp.task('sass',function(){
+    gulp.src('src/sass/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle:'compressed'}))
+        .pipe(renames('rola.min.css'))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('js',function(){
+    gulp.src('src/js/**/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(concats('all.js'))
+        .pipe(renames('rola.min.js'))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./dist/js'));
+});
